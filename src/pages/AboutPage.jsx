038402ac4d5264da595f22f52c55db6e5785b84a
farmerintech <farmerintech@gmail.com>
2026-07-02 22:19:04 +0100

    updated services

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -57,7 +57,7 @@ export default function AboutPage() {
         </div>
       </section>
       <ValuesBar />
-      <AssumptionsNote />
+      {/* <AssumptionsNote /> */}
     </PageTransition>
   );
 }
